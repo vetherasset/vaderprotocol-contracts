563c8fab305e09051f6ae1cb540230f984e1f18e
Satoshi Naoki <SenWebDev1009@gmail.com>
2021-06-24 16:50:59 +1000

    add deploy&verify script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+const secret = require("./.secret.json");
 
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
@@ -10,13 +13,46 @@ task("accounts", "Prints the list of accounts", async () => {
 });
 
 
- module.exports = {
-    solidity: {
-        version: "0.8.3",
-        settings: {
-          optimizer: {
-            enabled: true
-          }
-        }
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    mainnet: {
+      url: secret.mainnet.url,
+      accounts: [secret.privateKey]
+    },
+    ropsten: {
+      url: secret.ropsten.url,
+      accounts: [secret.privateKey]
+    },
+    kovan: {
+      url: secret.kovan.url,
+      accounts: [secret.privateKey]
+    },
+    rinkeby: {
+      url: secret.rinkeby.url,
+      accounts: [secret.privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.3",
+    settings: {
+      optimizer: {
+        enabled: true
       }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  },
+  etherscan: {
+    apiKey: secret.etherscanKey
+  },
 }
